14727e61f4a2b969d8707ed77b4380ea1974f0d4
Theethawat Savastham <5910110150@psu.ac.th>
2020-03-21 11:18:42 +0700

    Fetch Advice to show

--- src/Component/UIHandling.tsx
+++ src/Component/UIHandling.tsx
@@ -1,58 +1,64 @@
-import React, { Component } from "react";
-import IUserInterfaceChoice from "./IUserInterfaceChoice";
-import { UICategory } from "../Utility/UICategory";
-import AdviceDiseaseUI from "../UserInterface/AdviceDiseaseUI";
-import AdviceVitalSignUI from "../UserInterface/AdviceVitalSignUI";
-import DiseaseSource from "../UserInterface/DiseaseSourceUI";
-import RiskLevelUI from "../UserInterface/RiskLevelUI";
-import VitalSignStatUI from "../UserInterface/VitalSignStatUI";
-import DefaultUI from "../UserInterface/DefaultUI";
-import { VitalSignDataType } from "../Utility/VitalSignDataType";
-import { isNullOrUndefined } from "util";
+import React, { Component } from "react"
+import IUserInterfaceChoice from "./IUserInterfaceChoice"
+import { UICategory } from "../Utility/UICategory"
+import AdviceDiseaseUI from "../UserInterface/AdviceDiseaseUI"
+import AdviceVitalSignUI from "../UserInterface/AdviceVitalSignUI"
+import DiseaseSource from "../UserInterface/DiseaseSourceUI"
+import RiskLevelUI from "../UserInterface/RiskLevelUI"
+import VitalSignStatUI from "../UserInterface/VitalSignStatUI"
+import DefaultUI from "../UserInterface/DefaultUI"
+import { VitalSignDataType } from "../Utility/VitalSignDataType"
+import { isNullOrUndefined } from "util"
 
 class UIHandling extends Component<any, IUserInterfaceChoice> {
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            category: this.props.category,
-            selectDataType: this.props.selectDataType,
-            selectDisease: this.props.selectDisease
-        }
-        console.log("UI Handling Working")
-        console.log(this.state.selectDataType)
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      category: this.props.category,
+      selectDataType: this.props.selectDataType,
+      selectDisease: this.props.selectDisease
     }
+    console.log("UI Handling Working")
+    console.log(this.state.selectDataType)
+    this.renderUI = this.renderUI.bind(this)
+  }
 
-
-    renderUI(requestUI?: UICategory, dataType?: VitalSignDataType) {
-        console.log("At UI Handling RenderUI")
-        console.log(dataType)
-        let shouldShow
-        switch (requestUI) {
-            case UICategory.AdviceDisease: shouldShow = <AdviceDiseaseUI />
-                break;
-            case UICategory.AdviceVitalSign: shouldShow = <AdviceVitalSignUI selectDataType={dataType} />
-                break;
-            case UICategory.DiseaseSource: shouldShow = <DiseaseSource />
-                break;
-            case UICategory.RiskLevel: shouldShow = <RiskLevelUI />
-                break;
-            case UICategory.Default: shouldShow = <DefaultUI />
-                break;
-            default: shouldShow = <DefaultUI />
-                break;
-        }
-        return shouldShow
+  renderUI(requestUI?: UICategory, dataType?: VitalSignDataType) {
+    console.log("At UI Handling RenderUI")
+    console.log(dataType)
+    let shouldShow
+    switch (requestUI) {
+      case UICategory.AdviceDisease:
+        shouldShow = <AdviceDiseaseUI />
+        break
+      case UICategory.AdviceVitalSign:
+        shouldShow = <AdviceVitalSignUI selectDataType={dataType} />
+        break
+      case UICategory.DiseaseSource:
+        shouldShow = <DiseaseSource />
+        break
+      case UICategory.RiskLevel:
+        shouldShow = <RiskLevelUI />
+        break
+      case UICategory.Default:
+        shouldShow = <DefaultUI />
+        break
+      default:
+        shouldShow = <DefaultUI />
+        break
     }
+    return shouldShow
+  }
 
-    render() {
-        let mustHandlingUI: UICategory = this.props.category
-        let mustHandlingDataType: VitalSignDataType = isNullOrUndefined(this.props.selectDataType) ? VitalSignDataType.Unknown : this.props.selectDataType
-        var showingUI = this.renderUI(mustHandlingUI, mustHandlingDataType)
-        return (
-            <div>
-                {showingUI}
-            </div>
-        )
-    }
+  render() {
+    let mustHandlingUI: UICategory = this.props.category
+    let mustHandlingDataType: VitalSignDataType = isNullOrUndefined(
+      this.props.selectDataType
+    )
+      ? VitalSignDataType.Unknown
+      : this.props.selectDataType
+    var showingUI = this.renderUI(mustHandlingUI, mustHandlingDataType)
+    return <div>{showingUI}</div>
+  }
 }
 export default UIHandling
--- src/UserInterface/AdviceVitalSignUI.tsx
+++ src/UserInterface/AdviceVitalSignUI.tsx
@@ -1,85 +1,119 @@
-import React, { Component } from "react";
-import { VitalSignDataType } from "../Utility/VitalSignDataType";
+import React, { Component } from "react"
+import { VitalSignDataType } from "../Utility/VitalSignDataType"
 import Firebase from "../Firebase"
-import IVitalSignAdvice from "../Utility/Advicing/IVitalSignAdvice";
+import IVitalSignAdvice from "../Utility/Advicing/IVitalSignAdvice"
 let db = Firebase.firestore()
+/**
+ * Create by theethawat - 2020-03-21
+ */
 class AdviceVitalSignUI extends Component<any, any> {
-    constructor(props: any) {
-        super(props)
-        let dataTypeString: String = this.dataTypeToString(this.props.selectDataType)
-        this.state = {
-            dataTypeName: dataTypeString,
-            adviceDanger: "",
-            adviceRisk: "",
-            adviceSafe: ""
-        }
+  constructor(props: any) {
+    super(props)
+    let dataTypeString: String = this.dataTypeToString(
+      this.props.selectDataType
+    )
+    this.state = {
+      dataTypeName: dataTypeString,
+      adviceDanger: "",
+      adviceRisk: "",
+      adviceSafe: ""
     }
+    this.renderNewAdvice = this.renderNewAdvice.bind(this)
+  }
 
-    componentWillReceiveProps() {
-        let refreshDataTypeString = this.dataTypeToString(this.props.selectDataType)
-        console.log(this.props.selectDataType)
-        console.log("Set State")
+  componentWillReceiveProps(nextProps: any) {
+    let refreshDataTypeString = this.dataTypeToString(nextProps.selectDataType)
+    console.log(nextProps.selectDataType)
+    console.log("Set State")
+    this.fetchNewAdvice(refreshDataTypeString)
+    this.setState({
+      dataTypeName: refreshDataTypeString
+    })
+  }
+
+  dataTypeToString(dataType: VitalSignDataType) {
+    let outputString: string
+    if (dataType == VitalSignDataType.BloodPressure)
+      outputString = "blood_pressure"
+    else if (dataType == VitalSignDataType.Glucose) outputString = "glucose"
+    else if (dataType == VitalSignDataType.Spo2) outputString = "spo2"
+    else if (dataType == VitalSignDataType.HeartRate)
+      outputString = "heart_rate"
+    else outputString = "tba"
+    return outputString
+  }
+
+  fetchNewAdvice(dataTypeString: string) {
+    db.collection("vitalsign_advice")
+      .doc(dataTypeString)
+      .get()
+      .then(snapshot => {
+        let adviceObject = snapshot.data() as IVitalSignAdvice
+        let adviceDanger: string = adviceObject.advice_danger
+        let adviceRisk: string = adviceObject.advice_risk
+        let adviceSafe: string = adviceObject.advice_safe
+        console.log(adviceObject)
         this.setState({
-            dataTypeName: refreshDataTypeString
+          adviceDanger: adviceDanger,
+          adviceRisk: adviceRisk,
+          adviceSafe: adviceSafe
         })
-    }
+      })
+  }
 
-    dataTypeToString(dataType: VitalSignDataType) {
-        let outputString: String
-        if (dataType == VitalSignDataType.BloodPressure)
-            outputString = "blood_pressure"
-        else if (dataType == VitalSignDataType.Glucose)
-            outputString = "glucose"
-        else if (dataType == VitalSignDataType.Spo2)
-            outputString = "spo2"
-        else if (dataType == VitalSignDataType.HeartRate)
-            outputString = "heart_rate"
-        else
-            outputString = "tba"
-        return outputString
-    }
+  renderNewAdvice(event: any) {
+    let target = event.target
+    let adviceDangerCopy =
+      target.name == "advice_danger" ? target.value : this.state.adviceDanger
+    let adviceRiskCopy =
+      target.name == "advice_risk" ? target.value : this.state.adviceRisk
+    let adviceSafeCopy =
+      target.name == "advice_safe" ? target.value : this.state.adviceSafe
+    this.setState({
+      adviceDanger: adviceDangerCopy,
+      adviceRisk: adviceRiskCopy,
+      adviceSafe: adviceSafeCopy
+    })
+  }
 
-    componentDidMount() {
-        let dataTypeName = this.state.dataTypeName
-        db.collection("vitalsign_advice").doc(dataTypeName).get().then(snapshot => {
-            let adviceObject = snapshot.data() as IVitalSignAdvice
-            let adviceDanger: string = adviceObject.advice_danger
-            let adviceRisk: string = adviceObject.advice_risk
-            let adviceSafe: string = adviceObject.advice_safe
-            console.log(adviceObject)
-            this.setState({
-                adviceDanger: adviceDanger,
-                adviceRisk: adviceRisk,
-                adviceSafe: adviceSafe
-            })
-        })
-    }
-
-    renderState() {
-        let dataTypeName = this.state.dataTypeName
-        return (
-            <div>
-                <h4> การให้คำแนะนำเกี่ยวกับข้อมูลสุขภาพ </h4><hr />
-                <h5>ข้อมูลสุขภาพ {dataTypeName} </h5>
-                <form>
-                    <label>คำแนะนำสำหรับระยะปลอดภัย</label>
-                    <textarea className="form-control" rows={3} name="advice_safe" value={this.state.adviceSafe} ></textarea>
-                    <label>คำแนะนำสำหรับระยะเสี่ยง</label>
-                    <textarea className="form-control" rows={3} name="advice_risk" value={this.state.adviceRisk}></textarea>
-                    <label>คำแนะนำสำหรับระยะอันตราย</label>
-                    <textarea className="form-control" rows={3} name="advice_danger" value={this.state.adviceDanger}></textarea>
-                    <br />
-                    <button className="btn btn-primary" type="submit">Update</button>
-                </form>
-            </div>
-        )
-    }
-
-    render() {
-        let stateHTMLShow = this.renderState()
-        return (
-            stateHTMLShow
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h4> การให้คำแนะนำเกี่ยวกับข้อมูลสุขภาพ </h4>
+        <hr />
+        <h5>ข้อมูลสุขภาพ {this.state.dataTypeName} </h5>
+        <form>
+          <label>คำแนะนำสำหรับระยะปลอดภัย</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            onChange={this.renderNewAdvice}
+            name="advice_safe"
+            value={this.state.adviceSafe}
+          ></textarea>
+          <label>คำแนะนำสำหรับระยะเสี่ยง</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            onChange={this.renderNewAdvice}
+            name="advice_risk"
+            value={this.state.adviceRisk}
+          ></textarea>
+          <label>คำแนะนำสำหรับระยะอันตราย</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            onChange={this.renderNewAdvice}
+            name="advice_danger"
+            value={this.state.adviceDanger}
+          ></textarea>
+          <br />
+          <button className="btn btn-primary" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 export default AdviceVitalSignUI
